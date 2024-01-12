2fc9a2be8019fe6a473b04f35a65ba6c58d6633d
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-01-12 10:17:48 +0100

    Add js logic with html

--- logic.js
+++ logic.js
@@ -1,64 +1,50 @@
+const restartGameBtn = document.querySelector("#restart-game");
+const cells = document.querySelectorAll(".cell");
+const winner = document.querySelector("#winner");
+const showPoints = document.querySelector("#point-count");
+
+
+
+
 const Player = (name, symbol) => {
     let points = 0;
     const WinPoints = () => {
         points++;
-        console.log(`${name} has now ${points} points`);
+        showPoints.textContent = `${name} has now ${points} points`;
     }
     return { name, symbol, WinPoints };
 }
 
-const Gameboard = () => {
-    const row = 3;
-    const column = 3;
-    const gameboard = []; 
-
-    for(let i = 0; i < row; i++) {
-        gameboard[i] = [];
-        for(let j = 0; j < column; j++) {
-            gameboard[i].push([""]);
-        }
-    }
-
-    const PutSymbol = (symbol) => {
-        const row = prompt("which row to put your symbol? ");
-        const column = prompt("which column to put your symbol? ");
-        if (gameboard[row-1][column-1] != "X" && gameboard[row-1][column-1] != "O") {
-            gameboard[row-1][column-1] = symbol;
-            console.log(gameboard);
-            return false;
-        }
-        else {
-            console.log("spot already taken, try again");
-            return true;
-        }
-            
-        
-    }
-
-    return { gameboard, PutSymbol };
-}
-
 const Game = () => {
-    const board = Gameboard();
     const player1 = Player("Player 1", "X");
     const player2 = Player("Player 2", "O");
     let activePlayer = player1;
     let round = 0;
 
-    const PlayRound = () => {
+    cells.forEach((cell) => {
+        cell.addEventListener("click", () => {PutSymbol(cell, activePlayer.symbol);
+        });
+    });
+
+    const PutSymbol = (cell, symbol) => {
         round++;
-        while(board.PutSymbol(activePlayer.symbol)) {
-            board.PutSymbol(activePlayer.symbol);
-        }
-        if(CheckWin(activePlayer.symbol) == true) {
-            console.log(`Game done, ${activePlayer.name} wins!`);
-            return activePlayer.WinPoints();
+        console.log(round);
+        cell.textContent = symbol;
+        cell.disabled = true;
+        if(CheckWin(symbol) == true) {
+            cells.forEach((cell) => {
+                cell.disabled = true; 
+            });
+            winner.textContent = `Game done, ${activePlayer.name} wins!`;
+            return activePlayer.WinPoints();     
         };
         if (round === 9) {
-            return console.log(`Game done, this is a tie!`);
+            cells.forEach((cell) => {
+                cell.disabled = true; 
+            });
+            return winner.textContent = `Game done, this is a tie!`;
         }
-        SwitchActivePlayer();
-        PlayRound();
+        SwitchActivePlayer();           
     }
 
     const SwitchActivePlayer = () => {
@@ -70,19 +56,34 @@ const Game = () => {
     }
 
     const CheckWin = (symbol) => {
-        for(let i = 0; i < 3; i++) {
-            if((board.gameboard[i][0] == symbol && board.gameboard[i][0] == board.gameboard[i][1] && board.gameboard[i][1] == board.gameboard[i][2])
-                || (board.gameboard[0][i] == symbol && board.gameboard[0][i] == board.gameboard[1][i] && board.gameboard[1][i] == board.gameboard[2][i])) {
-                    return true;
-
+        for(let i = 1; i < 8; i+=3) {
+            if((document.getElementById(`${i}`).textContent == symbol && document.getElementById(`${i}`).textContent == document.getElementById(`${i+1}`).textContent && document.getElementById(`${i+1}`).textContent == document.getElementById(`${i+2}`).textContent)) {   
+                return true;
             }
         }
-        if(board.gameboard[1][1] == symbol && ((board.gameboard[0][0] == board.gameboard[1][1] && board.gameboard[1][1] == board.gameboard[2][2]) || (board.gameboard[2][0] == board.gameboard[1][1] && board.gameboard[1][1] == board.gameboard[0][2]))) {
+        for(let i = 1; i < 4; i++) {
+            if((document.getElementById(`${i}`).textContent == symbol && document.getElementById(`${i}`).textContent == document.getElementById(`${i+3}`).textContent && document.getElementById(`${i+3}`).textContent == document.getElementById(`${i+6}`).textContent)) {    
+                return true;
+            }
+        }
+        if((document.getElementById(`1`).textContent == symbol && (document.getElementById(`1`).textContent == document.getElementById(`5`).textContent && document.getElementById(`5`).textContent == document.getElementById(`9`).textContent)) 
+        || (document.getElementById(`7`).textContent == symbol && (document.getElementById(`7`).textContent == document.getElementById(`5`).textContent && document.getElementById(`5`).textContent == document.getElementById(`3`).textContent))) {
             return true
         }
         
     }
-    PlayRound();
+
+    restartGameBtn.addEventListener("click", () => {
+        cells.forEach((cell) => { 
+            cell.textContent = "";
+            cell.disabled = false;
+            activePlayer = player1;
+            round = 0;
+        });
+
+    });
 }
 
-Game(); 
+Game();
+
+
